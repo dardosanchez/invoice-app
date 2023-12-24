578102943481c4ea95d1b2f5b400c68ceb9dcda6
dardosanchez <dardosanchez2000@gmail.com>
2023-12-24 17:07:30 -0300

    Refactor

--- src/InvoiceApp.jsx
+++ src/InvoiceApp.jsx
@@ -7,13 +7,69 @@ import { TotalView } from "./components/TotalView.jsx";
 import { useState } from "react";
 
 export const InvoiceApp = () => {
-  const { id, name, client, company, items:itemsInitial , total} = getInvoice();
+  const {
+    id,
+    name,
+    client,
+    company,
+    items: itemsInitial,
+    total,
+  } = getInvoice();
 
-  const [productValue, setProductValue] = useState('');
-  const [priceValue, setPricetValue] = useState(0);
-  const [quantityValue, setQuantityValue] = useState(0);
+  const [formItemsState, setFormItemsState] = useState({
+    product: "",
+    price: "",
+    quantity: "",
+  });
+
+  const { product, price, quantity } = formItemsState;
 
   const [items, setItems] = useState(itemsInitial);
+  const [counter, setcounter] = useState(4);
+
+  const onInputChange = ({ target: { name, value } }) => {
+    {
+      setFormItemsState({
+        ...formItemsState,
+        [ name ]: value,
+      });
+    }
+  };
+
+  const onInvoiceItemsSubmit = (event) => {
+    {
+      event.preventDefault();
+
+      if (product.trim().length <= 1) return;
+      if (price.trim().length <= 1) return;
+      if (isNaN(price.trim())) {
+        alert("Error el precio no es un numero");
+        return;
+      }
+      if (quantity.trim().length < 1) {
+        alert("Error la cantidad no es un numero");
+        return;
+      }
+      if (isNaN(quantity.trim())) return;
+
+      setItems([
+        ...items,
+        {
+          id: counter,
+          product: product.trim(),
+          price: +price.trim(),
+          quantity: parseInt(quantity.trim(), 10),
+        },
+      ]);
+
+      setFormItemsState({
+        product: "",
+        price: "",
+        quantity: "",
+      });
+      setcounter(counter + 1);
+    }
+  };
 
   return (
     <>
@@ -33,26 +89,38 @@ export const InvoiceApp = () => {
             </div>
             <ListItemView name="Productos de la factura" items={items} />
             <TotalView total={total} />
-            
-            <form className="w-50" onSubmit={event => {
-              event.preventDefault();
-              setItems([...items, {key:4 ,product: productValue,price: priceValue,quantity: quantityValue}])
-            }}>
-              <input type="text" name="product" placeholder="Producto" className="form-control m-3" onChange={event => {
-                setProductValue(event.target.value);
-              }}/> 
-              <input type="text" name="price" placeholder="Precio" className="form-control m-3" onChange={event => {
-                setPricetValue(event.target.value);
-              }}/> 
-              <input type="text" name="quantity" placeholder="Cantidad" className="form-control m-3" onChange={event => {
-                setQuantityValue(event.target.value);
-              }}/> 
 
-              <button type="submit" className="btn btn-primary" >Crear Item</button>
+            <form className="w-50" onSubmit={onInvoiceItemsSubmit}>
+              <input
+                type="text"
+                name="product"
+                value={product}
+                placeholder="Producto"
+                className="form-control m-3"
+                onChange={onInputChange}
+              />
+              <input
+                type="text"
+                name="price"
+                value={price}
+                placeholder="Precio"
+                className="form-control m-3"
+                onChange={(event) => onInputChange(event)}
+              />
+              <input
+                type="text"
+                name="quantity"
+                value={quantity}
+                placeholder="Cantidad"
+                className="form-control m-3"
+                onChange={onInputChange}
+              />
+
+              <button type="submit" className="btn btn-primary m-3">
+                Nuevo Item
+              </button>
             </form>
-
           </div>
-          
         </div>
       </div>
     </>
