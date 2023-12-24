1c5f7a1e0fc70f5cbf9e1c16e079b2b709ff7efd
dardosanchez <dardosanchez2000@gmail.com>
2023-12-24 03:14:03 -0300

    UseState

--- src/InvoiceApp.jsx
+++ src/InvoiceApp.jsx
@@ -4,9 +4,16 @@ import { Companyview } from "./components/CompanyView.jsx";
 import { InvoiceView } from "./components/InvoiceView.jsx";
 import { ListItemView } from "./components/ListItemView.jsx";
 import { TotalView } from "./components/TotalView.jsx";
+import { useState } from "react";
 
 export const InvoiceApp = () => {
-  const { id, name, client, company, items , total} = getInvoice();
+  const { id, name, client, company, items:itemsInitial , total} = getInvoice();
+
+  const [productValue, setProductValue] = useState('');
+  const [priceValue, setPricetValue] = useState(0);
+  const [quantityValue, setQuantityValue] = useState(0);
+
+  const [items, setItems] = useState(itemsInitial);
 
   return (
     <>
@@ -26,7 +33,26 @@ export const InvoiceApp = () => {
             </div>
             <ListItemView name="Productos de la factura" items={items} />
             <TotalView total={total} />
+            
+            <form className="w-50" onSubmit={event => {
+              event.preventDefault();
+              setItems([...items, {key:4 ,product: productValue,price: priceValue,quantity: quantityValue}])
+            }}>
+              <input type="text" name="product" placeholder="Producto" className="form-control m-3" onChange={event => {
+                setProductValue(event.target.value);
+              }}/> 
+              <input type="text" name="price" placeholder="Precio" className="form-control m-3" onChange={event => {
+                setPricetValue(event.target.value);
+              }}/> 
+              <input type="text" name="quantity" placeholder="Cantidad" className="form-control m-3" onChange={event => {
+                setQuantityValue(event.target.value);
+              }}/> 
+
+              <button type="submit" className="btn btn-primary" >Crear Item</button>
+            </form>
+
           </div>
+          
         </div>
       </div>
     </>
